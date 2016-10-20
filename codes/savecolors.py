a5afebdbd3be49c8b85dad1a882ce399ee8881d1
Bhavin Joshi <bajoshi@asu.edu>
2016-10-20 14:10:18 -0700

    updates to savecolors.py

--- codes/savecolors.py
+++ codes/savecolors.py
@@ -15,8 +15,8 @@ def get_colors(matchedfile, threedn, threeds):
     pears_id = []
     fieldname = []
     threed_id = []
-    threedra = []
-    threeddec = []
+    pearsra = []
+    pearsdec = []
     ur_color = []
     stellarmass = []
     threed_z = []
@@ -28,8 +28,8 @@ def get_colors(matchedfile, threedn, threeds):
         pears_id.append(cat['pearsid'][i])
         fieldname.append(cat['field'][i])
         threed_id.append(cat['threed_id'][i])
-        threedra.append(cat['threedra'][i])
-        threeddec.append(cat['threeddec'][i])
+        pearsra.append(cat['pearsra'][i])
+        pearsdec.append(cat['pearsdec'][i])
         threed_z.append(cat['threed_zphot'][i])
         
         if cat['field'][i] == 'GOODS-N':
@@ -44,22 +44,26 @@ def get_colors(matchedfile, threedn, threeds):
     
     pears_id = np.array(pears_id)
     fieldname = np.array(fieldname)
+    pearsra = np.array(pearsra)
+    pearsdec = np.array(pearsdec)
     stellarmass = np.array(stellarmass)
     ur_color = np.array(ur_color)
     threed_id = np.array(threed_id)
     threed_z = np.array(threed_z)
 
-    return pears_id, fieldname, ur_color, stellarmass, threed_id, threed_z
+    return pears_id, fieldname, pearsra, pearsdec, ur_color, stellarmass, threed_id, threed_z
 
-def concat(pears_id, fieldname, ur_color, stellarmass, threed_id, threed_z, pid, fname, ur, ms, tid, tz):
+def concat(pears_id, fieldname, pearsra, pearsdec, ur_color, stellarmass, threed_id, threed_z, pid, fname, pra, pdec, ur, ms, tid, tz):
     pears_id = np.concatenate((pears_id,pid))
     fieldname = np.concatenate((fieldname,fname))
+    pearsra = np.concatenate((pearsra, pra))
+    pearsdec = np.concatenate((pearsdec, pdec))
     ur_color = np.concatenate((ur_color,ur))
     stellarmass = np.concatenate((stellarmass,ms))
     threed_id = np.concatenate((threed_id,tid))
     threed_z = np.concatenate((threed_z,tz))
     
-    return pears_id, fieldname, ur_color, stellarmass, threed_id, threed_z
+    return pears_id, fieldname, pearsra, pearsdec, ur_color, stellarmass, threed_id, threed_z
 
 if __name__ == '__main__':
 
@@ -93,18 +97,22 @@ if __name__ == '__main__':
     all_fields = [cdfn1, cdfn2, cdfn3, cdfn4, cdfs1, cdfs2, cdfs3, cdfs4, cdfs_new, cdfs_udf]
     # 2321 objects
 
-    pears_id, fieldname, ur_color, stellarmass, threed_id, threed_z = get_colors(cdfn1, threedn, threeds)
+    pears_id, fieldname, pearsra, pearsdec, ur_color, stellarmass, threed_id, threed_z = get_colors(cdfn1, threedn, threeds)
     # cdfn1 has to be done separately to get the concatenation to work
 
     for field in all_fields[1:]:
-        pid, fname, ur, ms, tid, tz = get_colors(field, threedn, threeds)
-        pears_id, fieldname, ur_color, stellarmass, threed_id, threed_z = concat(pears_id, fieldname, ur_color, stellarmass, threed_id, threed_z, pid, fname, ur, ms, tid, tz)
+        pid, fname, pra, pdec, ur, ms, tid, tz = get_colors(field, threedn, threeds)
+        pears_id, fieldname, pearsra, pearsdec, ur_color, stellarmass, threed_id, threed_z =\
+         concat(pears_id, fieldname, pearsra, pearsdec, ur_color, stellarmass, threed_id, threed_z, pid, fname, pra, pdec, ur, ms, tid, tz)
 
     outdir = home + '/Desktop/FIGS/stacking-analysis-pears/'
-    data = np.array(zip(pears_id, fieldname, ur_color, stellarmass, threed_id, threed_z),\
-                    dtype=[('pears_id', int), ('fieldname', '|S7'), ('ur_color', float), ('stellarmass', float), ('threed_id', int), ('threed_z', float)])
+    data = np.array(zip(pears_id, fieldname, pearsra, pearsdec, ur_color, stellarmass, threed_id, threed_z),\
+                    dtype=[('pears_id', int), ('fieldname', '|S7'), ('pearsra', float), ('pearsdec', float), ('ur_color', float), ('stellarmass', float), ('threed_id', int), ('threed_z', float)])
 
-    np.savetxt(outdir + 'color_stellarmass.txt', data, fmt=['%d', '%s', '%.2f', '%.2f', '%d', '%.2f'], delimiter=' ',\
+    np.savetxt(outdir + 'color_stellarmass.txt', data, fmt=['%d', '%s', '%.4f', '%.4f', '%.2f', '%.2f', '%d', '%.2f'], delimiter=' ',\
                header='The PEARS id is given here as only an integer.' + '\n' +\
                'The user must figure out if it is part of northern or southern fields.' + '\n' +\
-               'pearsid field urcol mstar threedid threedzphot')
+               'pearsid field pearsra pearsdec urcol mstar threedid threedzphot')
+
+    sys.exit(0)
+
