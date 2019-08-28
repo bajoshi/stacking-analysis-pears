c367e35ed6e6fb428a1005688cfe54a431381651
Bhavin Joshi <bajoshi@asu.edu>
2019-08-28 13:46:33 -0700

    Added proper filters for UVJ plotting and it seems to be fixed

--- stacking_pipeline/make_col_ms_plots.py
+++ stacking_pipeline/make_col_ms_plots.py
@@ -26,27 +26,9 @@ if 'firstlight' in os.uname()[1]:
 else:
     threedhst_datadir = '/Volumes/Bhavins_backup/3dhst_data/'
 
-def compute_fnu(filtername, spec_llam, spec_wav):
+def compute_fnu(spec_llam, spec_wav, filt_wav, filt_thru):
 
-    # Select filter curve based on filter name
-    # These are all from pysysnphot
-    # Wavelenght is in angstroms
-    # Throughput is an absolute fraction
-    if filtername == 'u_sdss':
-        band = pysynphot.ObsBandpass('sdss,u')
-    elif filtername == 'u_wfc3':
-        band = pysynphot.ObsBandpass('wfc3,uvis1,f336w')
-    elif filtername == 'v':
-        band = pysynphot.ObsBandpass('acs,wfc1,f555w')
-    elif filtername == 'r_sdss':
-        band = pysynphot.ObsBandpass('sdss,r')
-    elif filtername == 'j':
-        band = pysynphot.ObsBandpass('wfc3,ir,f125w')
-
-    filt_wav = band.wave
-    filt_thru = band.throughput
-
-    # NOw convert to f_nu before you do anything
+    # Now convert to f_nu before you do anything
     speed_of_light_ang = 2.99792458e18  # angstroms per second
 
     filt_nu = speed_of_light_ang / filt_wav
@@ -85,13 +67,16 @@ def compute_fnu(filtername, spec_llam, spec_wav):
     # Second, compute f_nu
     den = simps(y=filt_interp / spec_nu, x=spec_nu)
     num = simps(y=spec_fnu * filt_interp / spec_nu, x=spec_nu)
+
+    #den = simps(y=filt_interp, x=spec_nu)
+    #num = simps(y=spec_fnu * filt_interp, x=spec_nu)
     fnu = num / den
 
     #print "{:.3e}".format(num), "{:.3e}".format(den), "{:.3e}".format(fnu)
 
     return fnu
 
-def get_threed_ur(current_ra, current_dec, threed_ra, threed_dec, phot_cat_3dhst):
+def get_threed_match_idx(current_ra, current_dec, threed_ra, threed_dec):
 
     # Now match
     ra_lim = 0.3/3600  # arcseconds in degrees
@@ -134,6 +119,12 @@ def get_threed_ur(current_ra, current_dec, threed_ra, threed_dec, phot_cat_3dhst
         raise IndexError
         sys.exit(1)
 
+    return threed_phot_idx
+
+def get_threed_ur(current_ra, current_dec, threed_ra, threed_dec, phot_cat_3dhst):
+
+    threed_phot_idx = get_threed_match_idx(current_ra, current_dec, threed_ra, threed_dec)
+
     # Now get 3D-HST u-r color
     threed_uflux = float(phot_cat_3dhst['f_U'][threed_phot_idx])
     threed_rflux = float(phot_cat_3dhst['f_R'][threed_phot_idx])
@@ -146,6 +137,24 @@ def get_threed_ur(current_ra, current_dec, threed_ra, threed_dec, phot_cat_3dhst
     #print threed_phot_idx, "{:.3e}".format(threed_uflux), "{:.3e}".format(threed_rflux), "{:.3f}".format(threed_ur)
 
     return threed_ur
+
+def get_threed_uv_vj(current_ra, current_dec, threed_ra, threed_dec, phot_cat_3dhst):
+
+    threed_phot_idx = get_threed_match_idx(current_ra, current_dec, threed_ra, threed_dec)
+
+    # Now get 3D-HST u-v and v-j colors
+    threed_uflux = float(phot_cat_3dhst['f_U'][threed_phot_idx])
+    threed_vflux = float(phot_cat_3dhst['f_V'][threed_phot_idx])
+    threed_jflux = float(phot_cat_3dhst['f_J'][threed_phot_idx])
+
+    threed_umag = 25.0 - 2.5 * np.log10(threed_uflux)
+    threed_vmag = 25.0 - 2.5 * np.log10(threed_vflux)
+    threed_jmag = 25.0 - 2.5 * np.log10(threed_jflux)
+
+    threed_uv = threed_umag - threed_vmag
+    threed_vj = threed_vmag - threed_jmag
+
+    return threed_uv, threed_vj 
 
 def add_contours(x, y, ax):
 
@@ -185,7 +194,7 @@ def ur_ms_plots():
 
     # First get stellar mass and apply a cut to stellar mass
     ms = np.log10(cat['zp_ms'])
-    ms_idx = np.where((ms >= 9.0) & (ms <= 12.0))[0]  # Change the npy arrays to save and load accordingly
+    ms_idx = np.where((ms >= 8.0) & (ms <= 12.0))[0]  # Change the npy arrays to save and load accordingly
     print "Galaxies from stellar mass cut:", len(ms_idx)
 
     # Now get indices based on redshift intervals
@@ -197,7 +206,7 @@ def ur_ms_plots():
 
     # now loop over all galaxies within mass cut sample 
     # and get the u-r color for each galaxy
-    if not os.path.isfile(stacking_analysis_dir + 'ur_arr_9_logM_12.npy'):
+    if not os.path.isfile(stacking_analysis_dir + 'ur_arr_8_logM_12.npy'):
 
         ur = []
         threed_ur = []
@@ -224,14 +233,33 @@ def ur_ms_plots():
             usecols=(0,3,4, 9,10, 18,19, 30,31, 33,34, 39,40, 48,49, 54,55, 63,64, 15,16, 75,76, 78,79, 81,82, 84,85, 130,131,132,133), \
             skip_header=3)
 
+        # ------------------------------- Get required filter curves ------------------------------- # 
+        uvj_filt_dir = stacking_analysis_dir + 'filter_curves/'
+
+        # Wavelenght is in angstroms
+        # Throughput is an absolute fraction
+        u = np.genfromtxt(uvj_filt_dir + 'bessel_u.dat', dtype=None, names=['wav', 'filter_trans'])
+        r = np.genfromtxt(uvj_filt_dir + 'bessel_r.dat', dtype=None, names=['wav', 'filter_trans'])
+
+        """
+        When making the UVJ plot -- 
+        """
+        # The Bessel U and V filters are given in angstroms and fractions
+        u_filt_wav = u['wav']
+        u_filt_thru = u['filter_trans']
+
+        r_filt_wav = r['wav']
+        r_filt_thru = r['filter_trans']
+
+        # ------------------------------- Now begin looping over all stellar mass selected galaxies ------------------------------- #
         for idx in ms_idx:
             # First get teh full res model spectrum
             best_model_idx = cat[idx]['zp_model_idx']
             current_spec = model_comp_spec_llam_withlines_mmap[best_model_idx]
             
             # Now get the u and r magnitudes
-            ufnu = compute_fnu('u_sdss', current_spec, model_lam_grid_withlines_mmap)
-            rfnu = compute_fnu('r', current_spec, model_lam_grid_withlines_mmap)
+            ufnu = compute_fnu(current_spec, model_lam_grid_withlines_mmap, u_filt_wav, u_filt_thru)
+            rfnu = compute_fnu(current_spec, model_lam_grid_withlines_mmap, r_filt_wav, r_filt_thru)
             umag = -2.5 * np.log10(ufnu) - 48.60
             rmag = -2.5 * np.log10(rfnu) - 48.60
             current_ur = umag - rmag
@@ -390,6 +418,8 @@ def uvj():
 
         uv = []
         vj = []
+        threed_uv = []
+        threed_vj = []
 
         # Read model lambda grid # In agnstroms
         model_lam_grid_withlines_mmap = np.load(figs_dir + 'model_lam_grid_withlines_chabrier.npy', mmap_mode='r')
@@ -402,26 +432,56 @@ def uvj():
         # The combined grism+photometry fits, redshifts, and derived parameters are given in v4.1.5 (Momcheva et al. 2016)
         photometry_names = ['id', 'ra', 'dec', 'f_F160W', 'e_F160W', 'f_F435W', 'e_F435W', 'f_F606W', 'e_F606W', \
         'f_R', 'e_R', 'f_F775W', 'e_F775W', 'f_F850LP', 'e_F850LP', 'f_F125W', 'e_F125W', 'f_F140W', 'e_F140W', \
-        'f_U', 'e_U', 'f_IRAC1', 'e_IRAC1', 'f_IRAC2', 'e_IRAC2', 'f_IRAC3', 'e_IRAC3', 'f_IRAC4', 'e_IRAC4', \
+        'f_U', 'e_U', 'f_V', 'e_V', 'f_J', 'e_J', \
+        'f_IRAC1', 'e_IRAC1', 'f_IRAC2', 'e_IRAC2', 'f_IRAC3', 'e_IRAC3', 'f_IRAC4', 'e_IRAC4', \
         'IRAC1_contam', 'IRAC2_contam', 'IRAC3_contam', 'IRAC4_contam']
         goodsn_phot_cat_3dhst = np.genfromtxt(threedhst_datadir + 'goodsn_3dhst.v4.1.cat', \
             dtype=None, names=photometry_names, \
-            usecols=(0,3,4, 9,10, 15,16, 27,28, 30,31, 39,40, 45,46, 48,49, 54,55, 12,13, 63,64, 66,67, 69,70, 72,73, 90,91,92,93), \
+            usecols=(0,3,4, 9,10, 15,16, 27,28, 30,31, 39,40, 45,46, 48,49, 54,55, 12,13, 24,25, 51,52, 63,64, 66,67, 69,70, 72,73, 90,91,92,93), \
             skip_header=3)
         goodss_phot_cat_3dhst = np.genfromtxt(threedhst_datadir + 'goodss_3dhst.v4.1.cat', \
             dtype=None, names=photometry_names, \
-            usecols=(0,3,4, 9,10, 18,19, 30,31, 33,34, 39,40, 48,49, 54,55, 63,64, 15,16, 75,76, 78,79, 81,82, 84,85, 130,131,132,133), \
+            usecols=(0,3,4, 9,10, 18,19, 30,31, 33,34, 39,40, 48,49, 54,55, 63,64, 15,16, 24,25, 57,58, 75,76, 78,79, 81,82, 84,85, 130,131,132,133), \
             skip_header=3)
 
+        # ------------------------------- Get required filter curves ------------------------------- # 
+        uvj_filt_dir = stacking_analysis_dir + 'filter_curves/'
+
+        # Wavelenght is in angstroms
+        # Throughput is an absolute fraction
+        u = np.genfromtxt(uvj_filt_dir + 'bessel_u.dat', dtype=None, names=['wav', 'filter_trans'])
+        v = np.genfromtxt(uvj_filt_dir + 'bessel_v.dat', dtype=None, names=['wav', 'filter_trans'])
+        j = np.genfromtxt(uvj_filt_dir + 'nsfcam_jmk_trans.dat', dtype=None, names=['wav', 'filter_trans'], skip_header=1)
+
+        """
+        When making the UVJ plot -- 
+        """
+        # The Bessel U and V filters are given in angstroms and fractions
+        u_filt_wav = u['wav']
+        u_filt_thru = u['filter_trans']
+
+        v_filt_wav = v['wav']
+        v_filt_thru = v['filter_trans']
+
+        # SOme extra stuff needed for J band
+        j_filt_wav = j['wav'] * 1e4  # Convert microns to angstroms
+        j_filt_thru = j['filter_trans'] / 100.0  # They give transmission percentages 
+
+        # Now reverse because for some reason they provide the J band wavelengths in decending order
+        j_filt_wav = j_filt_wav[::-1]
+        j_filt_thru = j_filt_thru[::-1]
+
+        # ------------------------------- Now begin looping over all stellar mass selected galaxies ------------------------------- #
         for idx in ms_idx:
+            #print "\n", idx
             # First get teh full res model spectrum
             best_model_idx = cat[idx]['zp_model_idx']
             current_spec = model_comp_spec_llam_withlines_mmap[best_model_idx]
             
             # Now get the u and r magnitudes
-            ufnu = compute_fnu('u_wfc3', current_spec, model_lam_grid_withlines_mmap)
-            vfnu = compute_fnu('v', current_spec, model_lam_grid_withlines_mmap)
-            jfnu = compute_fnu('j', current_spec, model_lam_grid_withlines_mmap)
+            ufnu = compute_fnu(current_spec, model_lam_grid_withlines_mmap, u_filt_wav, u_filt_thru)
+            vfnu = compute_fnu(current_spec, model_lam_grid_withlines_mmap, v_filt_wav, v_filt_thru)
+            jfnu = compute_fnu(current_spec, model_lam_grid_withlines_mmap, j_filt_wav, j_filt_thru)
             umag = -2.5 * np.log10(ufnu) - 48.60
             vmag = -2.5 * np.log10(vfnu) - 48.60
             jmag = -2.5 * np.log10(jfnu) - 48.60
@@ -433,14 +493,47 @@ def uvj():
 
             #print umag, vmag, jmag, current_uv, current_vj
 
+            """
+            # Check the 3DHST colors as well
+            # They've used different filters so don't expect an exact match
+            current_ra = cat[idx]['RA']
+            current_dec = cat[idx]['DEC']
+            current_field = cat[idx]['Field']
+
+            # Assign catalogs 
+            if current_field == 'GOODS-N':
+                phot_cat_3dhst = goodsn_phot_cat_3dhst
+            elif current_field == 'GOODS-S':
+                phot_cat_3dhst = goodss_phot_cat_3dhst
+
+            threed_ra = phot_cat_3dhst['ra']
+            threed_dec = phot_cat_3dhst['dec']
+
+            current_threed_uv, current_threed_vj = get_threed_uv_vj(current_ra, current_dec, threed_ra, threed_dec, phot_cat_3dhst)
+            threed_uv.append(current_threed_uv)
+            threed_vj.append(current_threed_vj)
+
+            #print current_threed_uv, current_threed_vj
+            """
+
+        # Save colors
         uv = np.asarray(uv)
         vj = np.asarray(vj)
         np.save(stacking_analysis_dir + 'uv_arr_' + mass_str + '.npy', uv)
         np.save(stacking_analysis_dir + 'vj_arr_' + mass_str + '.npy', vj)
 
+        # Save threed colors as well
+        #threed_uv = np.asarray(threed_uv)
+        #threed_vj = np.asarray(threed_vj)
+        #np.save(stacking_analysis_dir + 'threed_uv_arr_' + mass_str + '.npy', threed_uv)
+        #np.save(stacking_analysis_dir + 'threed_vj_arr_' + mass_str + '.npy', threed_vj)
+
     else:
         uv = np.load(stacking_analysis_dir + 'uv_arr_' + mass_str + '.npy')
         vj = np.load(stacking_analysis_dir + 'vj_arr_' + mass_str + '.npy')
+
+    uv_plt = uv
+    vj_plt = vj
 
     # Get z intervals and their indices
     z_interval1_idx = np.where((zp >= 0.0) & (zp < 0.4))[0]
@@ -460,7 +553,7 @@ def uvj():
     # Define figure
     fig = plt.figure(figsize=(10, 5))
     gs = gridspec.GridSpec(10,12)
-    gs.update(left=0.05, right=0.95, bottom=0.05, top=0.95, wspace=0.0, hspace=0.3)
+    gs.update(left=0.05, right=0.95, bottom=0.05, top=0.95, wspace=0.25, hspace=0.3)
 
     # Put axes on grid
     ax1 = fig.add_subplot(gs[:5, :4])
@@ -476,20 +569,20 @@ def uvj():
     ax4.yaxis.set_label_coords(-0.12, 1.05)
 
     # Actual plotting 
-    ax1.scatter(vj[z_interval1_idx], uv[z_interval1_idx], s=1.5, color='k')
-    ax2.scatter(vj[z_interval2_idx], uv[z_interval2_idx], s=1.5, color='k')
-    ax3.scatter(vj[z_interval3_idx], uv[z_interval3_idx], s=1.5, color='k')
-    ax4.scatter(vj[z_interval4_idx], uv[z_interval4_idx], s=1.5, color='k')
-    ax5.scatter(vj[z_interval5_idx], uv[z_interval5_idx], s=1.5, color='k')
-    ax6.scatter(vj, uv, s=1.5, color='k')
+    ax1.scatter(vj_plt[z_interval1_idx], uv_plt[z_interval1_idx], s=1.5, color='k')
+    ax2.scatter(vj_plt[z_interval2_idx], uv_plt[z_interval2_idx], s=1.5, color='k')
+    ax3.scatter(vj_plt[z_interval3_idx], uv_plt[z_interval3_idx], s=1.5, color='k')
+    ax4.scatter(vj_plt[z_interval4_idx], uv_plt[z_interval4_idx], s=1.5, color='k')
+    ax5.scatter(vj_plt[z_interval5_idx], uv_plt[z_interval5_idx], s=1.5, color='k')
+    ax6.scatter(vj_plt, uv_plt, s=1.5, color='k')
 
     # Plot UVJ selection on each subplot
-    plot_uvj_selection(ax1)
-    plot_uvj_selection(ax2)
-    plot_uvj_selection(ax3)
-    plot_uvj_selection(ax4)
-    plot_uvj_selection(ax5)
-    plot_uvj_selection(ax6)
+    plot_uvj_selection(ax1, 0.0, 0.4)
+    plot_uvj_selection(ax2, 0.4, 0.7)
+    plot_uvj_selection(ax3, 0.7, 1.0)
+    plot_uvj_selection(ax4, 1.0, 2.0)
+    plot_uvj_selection(ax5, 2.0, 6.0)
+    plot_uvj_selection(ax6, 0.0, 6.0)
 
     # Add text 
     add_info_text_to_subplots(ax1, 0.0, 0.4, len(z_interval1_idx))
@@ -499,20 +592,20 @@ def uvj():
     add_info_text_to_subplots(ax5, 2.0, 6.0, len(z_interval5_idx))
     add_info_text_to_subplots(ax6, 0.0, 6.0, len(zp))
 
-    # Axes limits 
-    ax1.set_xlim(0.0, 3.0)
-    ax2.set_xlim(0.0, 3.0)
-    ax3.set_xlim(0.0, 3.0)
-    ax4.set_xlim(0.0, 3.0)
-    ax5.set_xlim(0.0, 3.0)
-    ax6.set_xlim(0.0, 3.0)
+    # Axes limits
+    ax1.set_xlim(-0.3, 2.5)
+    ax2.set_xlim(-0.3, 2.5)
+    ax3.set_xlim(-0.3, 2.5)
+    ax4.set_xlim(-0.3, 2.5)
+    ax5.set_xlim(-0.3, 2.5)
+    ax6.set_xlim(-0.3, 2.5)
 
-    ax1.set_ylim(-1.0, 2.5)
-    ax2.set_ylim(-1.0, 2.5)
-    ax3.set_ylim(-1.0, 2.5)
-    ax4.set_ylim(-1.0, 2.5)
-    ax5.set_ylim(-1.0, 2.5)
-    ax6.set_ylim(-1.0, 2.5)
+    ax1.set_ylim(0.0, 3.0)
+    ax2.set_ylim(0.0, 3.0)
+    ax3.set_ylim(0.0, 3.0)
+    ax4.set_ylim(0.0, 3.0)
+    ax5.set_ylim(0.0, 3.0)
+    ax6.set_ylim(0.0, 3.0)
 
     # Don't show x tick labels on the upper row
     ax1.set_xticklabels([])
@@ -529,15 +622,30 @@ def uvj():
 
     return None
 
-def plot_uvj_selection(ax):
+def plot_uvj_selection(ax, zlow, zhigh):
 
+    # Testing block
+    # Just to do a quick plot and check by eye
+    # Seems like the y_qg_line = 0.88*x_qg_line + 0.69
+    # works best for now in case of photo-z.
+    """ 
     x_qg_line = np.arange(0.0, 3.0, 0.1)  # qg stands for 'Quiescent Galaxies'
-    y_qg_line = 0.8*x_qg_line + 0.7
 
+    if zhigh < 0.5:
+        y_qg_line = 0.88*x_qg_line + 0.69
+    elif zlow >= 0.4 and zhigh <= 1.0:
+        y_qg_line = 0.88*x_qg_line + 0.59
+    else:  # for the bottom row
+        y_qg_line = 0.88*x_qg_line + 0.59
+
+    ax.plot(x_qg_line, y_qg_line)
+    """
+
+    # Depending on the given equation for the lines
     # I simply computed the min and max coordinates here by hand
-    ax.vlines(x=1.5, ymin=1.9, ymax=3.0, color='r')
-    ax.hlines(y=1.3, xmin=0.0, xmax=0.75, color='r')
-    ax.plot([0.75, 1.5], [1.3, 1.9], '-', color='r')
+    ax.vlines(x=1.6, ymin=2.1, ymax=3.0, color='r')
+    ax.hlines(y=1.3, xmin=-0.3, xmax=0.69, color='r')
+    ax.plot([0.69, 1.6], [1.3, 2.1], '-', color='r')
 
     return None
 
