27b17f93f29b009c3923d62ac530ab750781b1ad
Bhavin Joshi <bajoshi@asu.edu>
2020-06-22 16:40:24 -0400

    Added template modifying function for stellar velocity dispersion

--- stacking_pipeline/sims/template_mods.py
+++ stacking_pipeline/sims/template_mods.py
@@ -29,9 +29,55 @@ def redshift_spectrum(template_wav, template_llam, redshift):
 
     return redshifted_wav, redshifted_flux
 
+def add_stellar_vdisp(spec_wav, spec_flux, vdisp):
+
+    # Now compute the broadened spectrum by numerically
+    # integrating a Gaussian stellar velocity function
+    # with the stellar vdisp.
+    # Integration done numerically as a Riemann sum.
+
+    speed_of_light = 299792.458  # km per second
+    delta_v = 1.0
+
+    vdisp_spec = np.zeros(len(spec_wav))
+
+    print(len(spec_wav))
+
+    for w in range(len(spec_wav)):
+        print(w)
+        
+        lam = spec_wav[w]
+
+        I = 0
+        
+        # Now compute the integrand numerically
+        # between velocities that are within 
+        # +- 3-sigma using the specified velocity dispersion.
+        # Mean of all velocities should be 0,
+        # of course since the avg vel of all stars within
+        # a rotating disk should be zero.
+        for v in np.arange(-3*vdisp, 3*vdisp, delta_v):
+
+            beta = 1 + (v/speed_of_light)
+            new_lam = lam / beta
+            new_lam_idx = np.argmin(abs(spec_wav - new_lam))
+
+            flux_at_new_lam = spec_flux[new_lam_idx]
+
+            gauss_exp_func = np.exp(-1*v*v/(2*vdisp*vdisp))
+
+            prod = flux_at_new_lam * gauss_exp_func
+            I += prod
+
+        vdisp_spec[w] = I / (vdisp*np.sqrt(2*np.pi))
+
+    return vdisp_spec
+
 def lsf_convolve(spec):
 
-    mock_lsf = Gaussian1DKernel(8.0)
+    gauss_kernel_sigma = 8.0
+
+    mock_lsf = Gaussian1DKernel(gauss_kernel_sigma)
     lsf_convolved_spectrum = convolve(spec, mock_lsf, boundary='extend')
 
     return lsf_convolved_spectrum
@@ -81,14 +127,16 @@ def downsample(spec_wav, spec_flux):
 
     return resampled_flux, extended_grism_wav_grid
 
-def chop_spectrum(spec_wav, spec_flux, final_wav_grid):
+def chop_spectrum(spec_wav, spec_flux, chop_lim_low, chop_lim_high):
 
-    # Finally chop to required wavlength range
-    wav_start_idx = np.argmin(abs(spec_wav - final_wav_grid[0]))
-    wav_end_idx = np.argmin(abs(spec_wav - final_wav_grid[-1]))
-    grism_spec = spec_flux[wav_start_idx: wav_end_idx+1]
+    # Chop to required wavlength range
+    wav_start_idx = np.argmin(abs(spec_wav - chop_lim_low))
+    wav_end_idx = np.argmin(abs(spec_wav - chop_lim_high))
 
-    return grism_spec
+    short_wav = spec_wav[wav_start_idx: wav_end_idx+1]
+    short_spec = spec_flux[wav_start_idx: wav_end_idx+1]
+
+    return short_wav, short_spec
 
 def get_final_wav_grid():
 
@@ -116,8 +164,8 @@ def main():
     final_wav_grid = get_final_wav_grid()
     templates_with_mods = np.zeros((len(templates), len(final_wav_grid)))
 
-    #i_init = 7460
-    for i in range(len(templates)):
+    i_init = 8000
+    for i in range(i_init, len(templates)):
 
         current_template_name = templates['template_name'][i]
         current_redshift = templates['redshift'][i]
@@ -129,7 +177,19 @@ def main():
         current_template_wav = tt['wav']
         current_template_llam = tt['llam']
 
+        # For now randomly assign a stellar velocity dispersion
+        # value betwen 200 to 300 km/s 
+        stellar_vdisp_arr = np.linspace(200, 300, 11)
+        stellar_vdisp = np.random.choice(stellar_vdisp_arr, size=1)
+
+        # Decide how to shorten the BC03 spectra
+        # These wavelengths are in angstroms in the rest-frame
+        chop_lim_low = 2000
+        chop_lim_high = 15000
+
         # Mods. Steps:
+        # * First chop the spectrum so that the remaining computationally expensive steps 
+        # can be done relatively faster
         # * Redshift spectrum
         # * Convolve with a line spread function
         # * The BC03 models have no dust. Add in dust if needed according to the Calzetti prescription.
@@ -137,19 +197,21 @@ def main():
         # * Add random noise to each flux point.
         # * Convolve model with grism sensitivity curve. Downsample to grism resolution, 
         # while also adding in systematic noise... i.e., correlated flux measurements.
-        redshifted_wav, redshifted_flux = redshift_spectrum(current_template_wav, current_template_llam, current_redshift)
-        # = add_kinematics()
+
+        short_wav, short_spec = chop_spectrum(current_template_wav, current_template_llam, chop_lim_low, chop_lim_high)
+        redshifted_wav, redshifted_flux = redshift_spectrum(short_wav, short_spec, current_redshift)
+        vdisp_flux = add_stellar_vdisp(redshifted_wav, redshifted_flux, stellar_vdisp)
         # = add_dust()
         # = luminosity_func_mod()
-        spec_noise = add_statistical_noise(redshifted_flux)
-        lsf_convolved_spectrum = lsf_convolve(spec_noise)
-        grism_spec, extended_grism_wav_grid = downsample(redshifted_wav, lsf_convolved_spectrum)
-        final_spec = chop_spectrum(extended_grism_wav_grid, grism_spec, final_wav_grid)
-
-        """
+        #spec_noise = add_statistical_noise(vdisp_flux)
+        #lsf_convolved_spectrum = lsf_convolve(spec_noise)
+        #grism_spec, extended_grism_wav_grid = downsample(redshifted_wav, lsf_convolved_spectrum)
+        
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.plot(final_wav_grid, final_spec)
+        #ax.plot(current_template_wav, current_template_llam, color='k')
+        ax.plot(redshifted_wav, redshifted_flux)
+        ax.plot(redshifted_wav, vdisp_flux)
         ax.set_xscale('log')
         ax.set_xlim(5000, 10000)
         plt.show()
@@ -159,10 +221,9 @@ def main():
         plt.close()
 
         if i > i_init+20: sys.exit(0)
-        """
 
         # Add into numpy array
-        templates_with_mods[i] = final_spec
+        #templates_with_mods[i] = final_spec
 
     np.save(figs_dir + "modified_templates.npy", templates_with_mods)
 
