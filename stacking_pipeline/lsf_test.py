8d5163af5cc86e75840f1387bd164abafcf07187
Bhavin Joshi <bajoshi@asu.edu>
2020-10-17 00:30:31 -0400

    Progress with LSF testing

--- stacking_pipeline/lsf_test.py
+++ stacking_pipeline/lsf_test.py
@@ -1,5 +1,11 @@
 import numpy as np
 import scipy
+from astropy.modeling import models
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
 
 def main():
     """
@@ -47,12 +53,80 @@ def main():
     """
 
     # Create a dummy spectrum
-    spec = np.ones(1000)
+    spec = np.zeros(1000)
+    wav = np.arange(len(spec))
 
     # Superimpose an emission line on the spectrum
+    g = models.Gaussian1D(amplitude=3, mean=300.0, stddev=20.0)
+    gauss_1d_emission = g(wav)
 
+    spec = spec + gauss_1d_emission
 
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(wav, spec)
+    plt.show()
+    """
 
+    # Now assign some width to the dummy galaxy
+    # in pixels
+    # say 1-sigma if it is shaped like a Gaussian
+    # will deal with Sersic and other complicated profiles later
+    galaxy_width = 5
+    num_sigma = 1  # e.g., going from -2 to +2 sigma of galaxy width
+
+    # Assume that each time the spectrum shifts 
+    # by some amount epsilon. I'm leaving this param
+    # free for now.
+    # in units of wavelength
+    # this is saying that if a given pixel has the center of
+    # the emission line at lambda then the adjacent pixel along
+    # the dispersion direction will have the line center at
+    # lambda + epsilon
+    epsilon = 10.0
+    # BE CAREFUL!! This currently means epsilon steps of wavelength
+    # so that if the wavelenght is sampled at 1 A then epsilon is
+    # 30 A but if the wavelength array is sampled at 10 A then epsilon
+    # means 300 A.
+
+    print("Width of galaxy provided:", galaxy_width)
+    print("Will consider between +- these many sigmas:", num_sigma)
+    print("Epsilon:", epsilon)
+
+    exten = int(epsilon * num_sigma * galaxy_width)
+    print("Extension:", exten)
+    total_pix = num_sigma * galaxy_width
+    print("Total pixels to consider:", total_pix)
+
+    final_spec = np.zeros(shape=(total_pix, (len(spec) + exten)))
+    print("Shape of final spectra array:", final_spec.shape)
+    print("\n")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for pix in range(total_pix):
+
+        # Create shifted spectrum
+        i = int(epsilon*(1+pix))  # starting index for shift
+        shifted_spec = np.zeros(len(spec) + i)
+        shifted_spec[i:] = spec
+
+        print("\nAt pixel:", pix+1)
+        print("Shifting to index:", i)
+        print("len of shifted spec:", len(shifted_spec))
+
+        # Now fold it into the final spectrum
+        final_spec[pix, :len(shifted_spec)] = shifted_spec
+
+        ax.plot(shifted_spec)
+
+    final_spec_comb = np.sum(final_spec, axis=0)
+    print("Shape of final LSF broadened spectrum:", final_spec_comb.shape)
+
+    ax.plot(final_spec_comb, color='k')
+    plt.show()
 
     return None
 
